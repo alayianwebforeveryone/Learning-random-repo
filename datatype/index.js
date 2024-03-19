d2b1b5df55646ba4d290630f0d6462833a006d23
alayianwebforeveryone <alayiansaeed@gmail.com>
2024-03-19 23:48:09 +0500

    add datatype

---
+++ datatype/index.js
@@ -0,0 +1,60 @@
+let name = "alayian"
+// console.log(name);
+
+// datatypes
+
+
+let age = "alayian"
+ age = 20;
+//  console.log(typeof age, age)
+ age = String(40)
+
+ let userName= "ali";
+//  console.log( typeof userName)
+ userName = Number("ali")
+//  console.log(typeof userName)
+
+//  console.log(20+"30")
+//  console.log("20"+"30")
+//  console.log("20"+30)
+//  console.log(20+30)
+//  console.log(2+ true)
+//  console.log(2+ false)
+//  console.log(2+ null)
+//  console.log(2+ undefined)
+//  console.log(10+"30")
+ 
+ 
+ 
+ let s ;
+ s = undefined;
+ let p = null;
+ let someBool = true;
+//  primitive
+//  let symbolVar = Symbol("unique")
+//  console.log(typeof someBool ,someBool)
+//  console.log(typeof name, name)
+//  console.log(typeof s, s)
+//  console.log(typeof p, p)
+//  console.log(typeof symbolVar, symbolVar)
+//  console.log(typeof age, age)
+
+//  Non primitive
+// Array, Object, function
+
+
+
+
+
+//  console.log(s)
+
+//   s = null
+ //  Array
+ const myArr = ["user", 2, "name"] 
+       console.log( myArr.length)  
+        const index_3 = myArr[2]
+        console.log("🚀 ~ index_3:", index_3)
+        myArr[3] = "alayian"
+       
+         
+ console.log(myArr)
